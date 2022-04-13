4866a60afd038445eac45ee4a894adb709d5f119
Ivan St <102698630+HebaruDev@users.noreply.github.com>
2022-04-13 17:47:56 +0200

    Update AutoRead.py

--- AutoRead.py
+++ AutoRead.py
@@ -4,25 +4,25 @@ from asyncio import sleep
 @loader.tds 
 class AutoReadMod(loader.Module): 
     """Модуль, который показывает, что ты в сети. Сам читает все сообщения в чатах, каналах и лс.""" 
-    strings = {'name': 'AutoRead'} 
+    strings = {'name': 'Auto read'} 
  
     async def client_ready(self, client, db): 
         self.db = db 
  
-    async def onlinecmd(self, message): 
-        """Включить/выключить режим.""" 
-        if not self.db.get("Eternal Online", "status"): 
-            self.db.set("Eternal Online", "status", True) 
+    async def autoreadcmd(self, message): 
+        """Включить/выключить режим AutoRead.""" 
+        if not self.db.get("Auto read", "status"): 
+            self.db.set("Auto read", "status", True) 
             await message.edit("<b>[AutoRead]</b> Включён.") 
-            while self.db.get("Eternal Online", "status"): 
+            while self.db.get("Auto read", "status"): 
                 msg = await message.client.send_message("me", "Telegram best messenger!")
                 await msg.delete()
                 await sleep(60) 
  
         else: 
-            self.db.set("Eternal Online", "status", False) 
+            self.db.set("Auto read", "status", False) 
             await message.edit("<b>[AutoRead]</b> Выключен.")
 
     async def watcher(self, message): 
-        if self.db.get("Eternal Online", "status"):
+        if self.db.get("Auto read", "status"):
             await message.client.send_read_acknowledge(message.chat_id, clear_mentions=True)
